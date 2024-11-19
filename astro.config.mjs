a96ff70ea100335e7417173d5d4fea579707c2b8
Vitor Pinto Da Silva <silva.vitoremanuel@outlook.com>
2024-11-19 15:54:09 +0100

    ending of tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://vitpinto-blog.netlify.app/"
+  site: "https://vitpinto-blog.netlify.app/",
+  integrations: [preact()]
 });
